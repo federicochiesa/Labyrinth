dd9007a2af8c5dfbadcb0fc6ea2630a4b27ffe62
Federico Chiesa <federico.chiesa@me.com>
2023-04-28 16:34:10 +0200

    add new maze, fix drawing bug

--- sequential.cpp
+++ sequential.cpp
@@ -20,7 +20,7 @@ struct Ball
 int main()
 {
     // Parameters
-    std::string filename = "maze"; // Set this to the filename of the maze to solve.
+    std::string filename = "maze1"; // Set this to the filename of the maze to solve.
     int numberOfBalls = 100;
     int startX = 213;
     int startY = 3;
@@ -28,9 +28,9 @@ int main()
     std::string maze;
     std::string textLine;
     std::ifstream fileReader((filename + ".pgm"));
-    std::random_device rd;     
-    std::mt19937 rng(rd());    
-    std::uniform_int_distribution<int> uni(-4,4); 
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> uni(-3,3);
     std::string word;
     std::vector<std::string> mazeVector;
 
@@ -56,7 +56,6 @@ int main()
     }
 
     int imageWidth = std::stoi(mazeVector[IMAGE_WIDTH_INDEX]);
-    int imageHeight = std::stoi(mazeVector[IMAGE_HEIGHT_INDEX]);
     std::vector<Ball> ballArray;
 
     for(int i = 0; i < numberOfBalls; i++){
@@ -76,7 +75,7 @@ int main()
         for(int i = 0; i < ballArray.size(); i++){
             int nextX = ballArray[i].x.back() + uni(rng);
             int nextY = ballArray[i].y.back() + uni(rng);
-            while(mazeVector[nextY * (imageWidth - 1) + nextX] == "0"){
+            while(mazeVector[nextY * (imageWidth - 1) + nextX] == "0" || nextX < 0 || nextY < 0){
                 nextX = ballArray[i].x.back() + uni(rng);
                 nextY = ballArray[i].y.back() + uni(rng);
             }
@@ -86,7 +85,7 @@ int main()
                 finisher = &ballArray[i];
                 std::cout << "vinto";
                 for(int j = 0; j < ballArray[i].x.size(); j++){
-                    mazeVector[ballArray[i].x[j] * (imageWidth - 1) + ballArray[i].y[j]] = "100";
+                    mazeVector[ballArray[i].y[j] * (imageWidth - 1) + ballArray[i].x[j]] = "100";
                 }
                 
                 std::ofstream file("result.pgm");
@@ -94,14 +93,14 @@ int main()
                     file << mazeVector[0] + "\n" + mazeVector[1] + " " + mazeVector[2] + "\n" + mazeVector[3] + "\n";
                     std::string result;
                     for(int k = 4; k < mazeVector.size(); k++){
-                        result += word + " ";
+                        result += mazeVector[k] + " ";
                     }
                     file << result;
                     file.close();
                 }
+                return 0;
             }
         }
     }
-    std::cout << "finito!";
     return 0;
 }
