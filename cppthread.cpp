ab7228adcad99b50e6fcaeb405d203d16bc8e604
Federico Chiesa <federico.chiesa@me.com>
2023-05-02 10:56:35 +0200

    add time measure of first finisher

--- cppthread.cpp
+++ cppthread.cpp
@@ -6,11 +6,15 @@
 #include <chrono>
 #include <random>
 #include <thread>
+#include <mutex>
 
 #define FILE_HEADER 0
 #define IMAGE_WIDTH_INDEX 1
 #define IMAGE_HEIGHT_INDEX 2
 #define FILE_START_OFFSET 4
+
+bool finished = false;
+std::mutex mutex;
 
 struct Ball
 {
@@ -39,40 +43,40 @@ void mazeSolver(int numberOfBalls, std::vector<std::string> mazeVector, int star
     }
     Ball p;
     Ball *finisher = &p;
+    finisher = NULL;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    int num_finished = 0;
-    while (num_finished < numberOfBalls){
+    while(finisher == NULL && finished == false){
         for(int i = 0; i < ballArray.size(); i++){
-            if (ballArray[i].arrived == false)
-            {
-                int nextX = ballArray[i].x.back() + uni(rng);
-                int nextY = ballArray[i].y.back() + uni(rng);
-                while(nextX < 0 || nextY < 0 || mazeVector[nextY * (imageWidth - 1) + nextX] == "0"){
-                    nextX = ballArray[i].x.back() + uni(rng);
-                    nextY = ballArray[i].y.back() + uni(rng);
-                }
-                ballArray[i].x.push_back(nextX);
-                ballArray[i].y.push_back(nextY);
-                if(mazeVector[nextY * (imageWidth - 1) + nextX] != "0" && mazeVector[nextY * (imageWidth - 1) + nextX] != "255"){
-                    finisher = &ballArray[i];
-                    num_finished++;
-                    ballArray[i].arrived = true;
-                    for(int j = 0; j < ballArray[i].x.size(); j++)
-                        mazeVector[ballArray[i].y[j] * (imageWidth - 1) + ballArray[i].x[j]] = "100";
-                    std::ofstream file("result.pgm");
-                    if (file.is_open()) {
-                        file << mazeVector[0] + "\n" + mazeVector[1] + " " + mazeVector[2] + "\n" + mazeVector[3] + "\n";
-                        std::string result;
-                        for(int k = 4; k < mazeVector.size(); k++){
-                            result += mazeVector[k] + " ";
-                        }
-                        file << result;
-                        file.close();
+            int nextX = ballArray[i].x.back() + uni(rng);
+            int nextY = ballArray[i].y.back() + uni(rng);
+            while(nextX < 0 || nextY < 0 || mazeVector[nextY * (imageWidth - 1) + nextX] == "0"){
+                nextX = ballArray[i].x.back() + uni(rng);
+                nextY = ballArray[i].y.back() + uni(rng);
+            }
+            ballArray[i].x.push_back(nextX);
+            ballArray[i].y.push_back(nextY);
+            if(mazeVector[nextY * (imageWidth - 1) + nextX] != "0" && mazeVector[nextY * (imageWidth - 1) + nextX] != "255"){
+                finisher = &ballArray[i];
+                //Finished
+                mutex.lock();
+                finished = true;
+                mutex.unlock();
+                std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+                auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+                std::cout << "Finished in " << time << "ms";
+                for(int j = 0; j < ballArray[i].x.size(); j++)
+                    mazeVector[ballArray[i].y[j] * (imageWidth - 1) + ballArray[i].x[j]] = "100";
+                std::ofstream file("result.pgm");
+                if (file.is_open()) {
+                    file << mazeVector[0] + "\n" + mazeVector[1] + " " + mazeVector[2] + "\n" + mazeVector[3] + "\n";
+                    std::string result;
+                    for(int k = 4; k < mazeVector.size(); k++){
+                        result += mazeVector[k] + " ";
                     }
-                    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-                    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-                    std::cout << "Parallel Time = " << time << "[ms] " << i << std::endl;
+                    file << result;
+                    file.close();
                 }
+                return;
             }
         }
     }
@@ -84,9 +88,9 @@ int main(){
     std::thread myThreads[numberOfThreads];
     std::string filename = "maze1"; // Set this to the filename of the maze to solve.
     int numberOfBalls = 10;
-    int startX = 213; // Starting X pixel counting from the left
+    int startX = 463; // Starting X pixel counting from the left
     int startY = 3; // Starting X pixel counting from the top
-
+    
     std::string maze;
     std::string textLine;
     std::ifstream fileReader((filename + ".pgm"));
